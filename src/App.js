6aca30163037feefec90e2c02a8b6b11ecc1a7ee
kaiihudson <sebaror1@gmail.com>
2018-11-28 14:37:50 -0300

    we work on react

--- src/App.js
+++ src/App.js
@@ -1,28 +1,80 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+//import ReactDOM from 'react-dom'
 import './App.css';
+class App extends React.Component {
+  constructor(props){
+      super(props);
+      this.state={value: '', setCard: null, searchT: null};
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleChange(event){
+      this.setState({value: event.target.value});
+  }
+  handleSubmit(event){
+      event.preventDefault();
+      console.log("You've pressed a button");
+      this.setState({setCard: this.state.value})
+  }
 
-class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Are you afraid... Potter?
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <form onSubmit={this.handleSubmit}>
+              <input
+                  type="text"
+                  name="card"
+                  placeholder='Which card are you looking for?'
+                  value={this.state.value}
+                  onChange={this.handleChange}
+              />
+              <input type="submit" value="Submit" />
+              {this.state.setCard &&
+              <p> you've chosen {this.state.setCard}</p> &&
+              <Table />
+              }
+
+          </form>
         </header>
       </div>
     );
   }
+}
+class Table extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            cards: [],
+        }    }
+    componentDidMount(): void {
+        fetch('https://api.magicthegathering.io/v1/cards/',)
+        .then(results => {
+            return results.json();
+        }).then(data =>{
+            let cardsR = data.results.map((card) =>{
+                return(
+                    <p key={card.results}>
+                        <li key={card.cards.name} />
+                    </p>
+                )
+            });
+            this.setState({cards: cardsR});
+            console.log("state", this.state.cards)
+        })
+    }
+    render(){
+        return(
+            <div className="container2">
+                <div className="container1">
+                    {this.state.cards}
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
