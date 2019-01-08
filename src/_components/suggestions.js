2906833bcdbb35fd06feba1565a92787fff9f6df
kaiihudson <sebaror1@gmail.com>
2019-01-08 11:42:47 -0300

    async call seems to be ok, yet it's not generating a state of actual content. still empty. FIX?

--- src/_components/suggestions.js
+++ src/_components/suggestions.js
@@ -3,31 +3,52 @@ import React from "react";
 
 import suggestionGet from '../_resources/importer'
 
+/* --------------- */
+/*    Component    */
+/* --------------- */
 
-// When suggestion is clicked, Autosuggest needs to populate the input
-// based on the clicked suggestion. Teach Autosuggest how to calculate the
-// input value for every given suggestion.
-const getSuggestionValue = suggestion => suggestion.name;
+function getSuggestionValue(suggestion) {
+    return suggestion.name;
+}
 
-// Use your imagination to render suggestions.
-const renderSuggestion = suggestion => (
-    <div>
-        <div>
-            {suggestion.name}
-        </div>
-    </div>
-);
+function renderSuggestion(suggestion) {
+    return (
+        <span>{suggestion.name}</span>
+    );
+}
 
 class SuggestName extends React.Component {
     constructor() {
         super();
+
         this.state = {
             value: '',
-            suggestions: []
+            suggestions: [],
+            isLoading: false
         };
+
+        this.lastRequestId = null;
     }
 
+    loadSuggestions(value) {
+        let suggestionGrab = suggestionGet(value);
+        // Cancel the previous request
+        if (this.lastRequestId !== null) {
+            clearTimeout(this.lastRequestId);
+        }
 
+        this.setState({
+            isLoading: true
+        });
+
+        // Fake request
+        this.lastRequestId = setTimeout(() => {
+            this.setState({
+                isLoading: false,
+                suggestions: suggestionGrab
+            });
+        }, 1000);
+    }
 
     onChange = (event, { newValue }) => {
         this.setState({
@@ -35,14 +56,10 @@ class SuggestName extends React.Component {
         });
     };
 
-    // Autosuggest will call this function every time you need to update suggestions.
-    // You already implemented this logic above, so just use it.
     onSuggestionsFetchRequested = ({ value }) => {
-        let suggestionResponse = suggestionGet(value);
-        this.setState({suggestions: suggestionResponse})
+        this.loadSuggestions(value);
     };
 
-    // Autosuggest will call this function every time you need to clear suggestions.
     onSuggestionsClearRequested = () => {
         this.setState({
             suggestions: []
@@ -50,29 +67,27 @@ class SuggestName extends React.Component {
     };
 
     render() {
-        console.log(suggestionGet(this.state.value));
-
-        const { value, suggestions } = this.state;
-
-        // Autosuggest will pass through all these props to the input.
+        const { value, suggestions, isLoading } = this.state;
         const inputProps = {
-            placeholder: 'Type a card name',
+            placeholder: "Start Typing a card...",
             value,
             onChange: this.onChange
         };
+        const status = (isLoading ? 'Loading...' : 'Type to load suggestions');
 
-        // Finally, render it!
         return (
-            <Autosuggest
-                suggestions={suggestions}
-                onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-                onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-                getSuggestionValue={getSuggestionValue}
-                renderSuggestion={renderSuggestion}
-                inputProps={inputProps}
-                focusInputOnSuggestionClick={false}
-                highlightFirstSuggestion={true}
-            />
+            <div>
+                <div className="status">
+                    <strong>Status:</strong> {status}
+                </div>
+                <Autosuggest
+                    suggestions={suggestions}
+                    onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+                    onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+                    getSuggestionValue={getSuggestionValue}
+                    renderSuggestion={renderSuggestion}
+                    inputProps={inputProps} />
+            </div>
         );
     }
 }
--- src/_resources/importer.js
+++ src/_resources/importer.js
@@ -1,6 +1,3 @@
-// query by uniques
-
-
 
 // Get all cards with a name cardName
 export default function suggestionGet(cardName){
@@ -9,19 +6,37 @@ export default function suggestionGet(cardName){
     let cardLength = cardName.length;
     // noise reduction
     // enter the function only if the lenght of the search string is greater than 3
-    if (cardLength >= 3){
+    if (cardLength >= 2){
         //start fetch
-    fetch(`https://api.magicthegathering.io/v1/cards/?name=${cardName}`)
-    // turn rawData into a JSON
-        .then(rawData => rawData.json())
-        // turn JSON into an Array for AutoSuggestReact.js to read it
-        .then(jsonData => {
-           suggestionArray = jsonData;
-           console.log(suggestionArray)
-        })}
+        let suggestionArray = fetch(`https://api.magicthegathering.io/v1/cards/?name=${cardName}`)
+        // turn rawData into a JSON
+            .then(rawData => rawData.json())
+            // turn JSON into an Array for AutoSuggestReact.js to read it
+            .then(jsonData => {
+                suggestionArray = jsonData;
+                console.log(suggestionArray)
+            })}
+    // async example functions
+    function getMatchingCard(cardName) {
+        const escapedValue = escapeRegexCharacters(cardName.trim());
+
+        if (escapedValue === '') {
+            return suggestionArray = [];
+        }
+
+        const regex = new RegExp('^' + escapedValue, 'i');
+
+        return suggestionArray.filter(card => regex.test(card.cards.name));
+    }
+    function escapeRegexCharacters(str) {
+        return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+    }
+
     //return empty if no response and return the cardNames if there's response
-    return cardLength === 0 ? [] : suggestionArray.filter(card =>
-                card.cards.name.toLowerCase().slice(0, cardLength) === cardName
-            )
+    return getMatchingCard(cardName)
 }
+
+
+
+
 
